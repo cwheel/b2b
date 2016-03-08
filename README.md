7a12357ec0c8d591b7f644627a7adb820c92fb4a
Cameron Wheeler <cameron@scelos.com>
2016-03-07 21:09:54 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# b2b
+##### Back to Blue
+
+UMass Spire recently received a less than beautiful red top bar. A Chrome extension, b2b makes Spire blue again and saves your eyes from unsightly color clashes.
---
+++ src/inject/inject.js
@@ -0,0 +1,4 @@
+$("#banner").attr('background', '');
+$($($($($("body").children()[0]).children()[0]).children()[0]).children()[0]).css('background', '#455b87');
+$($($("#logo").children()[0]).children()[0]).attr('src',  chrome.extension.getURL('src/inject/blue_logo.png'));
+$("#pthdr2logimg").attr('src',  chrome.extension.getURL('src/inject/blue_logo_small.png'))
